ac984b7b0ae7f0a905bd7550b97b644c28ce9bd5
dheer152004 <dheerajkumar152004@outlook.com>
2026-05-09 18:58:31 +0530

    content_model added

---
+++ lib/models/content_models.dart
@@ -0,0 +1,71 @@
+class ApiCategory {
+  final int id;
+  final String name;
+  final int wordCount;
+
+  const ApiCategory({
+    required this.id,
+    required this.name,
+    required this.wordCount,
+  });
+
+  factory ApiCategory.fromJson(Map<String, dynamic> json) {
+    return ApiCategory(
+      id: _readInt(json['id']),
+      name: json['name']?.toString() ?? '',
+      wordCount: _readInt(json['wordCount']),
+    );
+  }
+}
+
+class ApiWord {
+  final int id;
+  final String word;
+  final String meaning;
+  final String memeImageUrl;
+  final String categoryName;
+  final List<String> examples;
+
+  const ApiWord({
+    required this.id,
+    required this.word,
+    required this.meaning,
+    required this.memeImageUrl,
+    required this.categoryName,
+    required this.examples,
+  });
+
+  factory ApiWord.fromJson(Map<String, dynamic> json) {
+    final rawExamples = json['examples'];
+    final examples = rawExamples is List
+        ? rawExamples.map((entry) => entry.toString()).toList()
+        : <String>[];
+
+    return ApiWord(
+      id: _readInt(json['id']),
+      word: json['word']?.toString() ?? '',
+      meaning: json['meaning']?.toString() ?? '',
+      memeImageUrl: json['memeImageUrl']?.toString() ?? '',
+      categoryName: json['categoryName']?.toString() ?? '',
+      examples: examples,
+    );
+  }
+
+  String get primaryExample => examples.isNotEmpty ? examples.first : '';
+}
+
+int _readInt(Object? value) {
+  if (value is int) {
+    return value;
+  }
+
+  if (value is String) {
+    return int.tryParse(value) ?? 0;
+  }
+
+  if (value is num) {
+    return value.toInt();
+  }
+
+  return 0;
+}
